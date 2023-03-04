616a5d663b86cd4d90fc9b3320d221a5020b0039
ryegrost8 <rafael.yegros@bit4b.com.br>
2023-03-04 13:19:58 -0300

    build: build to use git in package.json's

---
+++ dist/cli/deploy.js
@@ -0,0 +1,46 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const yargs_1 = require("yargs");
+const config_1 = require("../config");
+const adapter_1 = __importDefault(require("../adapter"));
+exports.command = 'deploy [services]';
+exports.desc = 'Dynamically identifies changes in your cds data model and deploys them to the database';
+exports.builder = {
+    service: {
+        alias: 's',
+        type: yargs_1.array,
+        default: ['db'],
+    },
+    'auto-undeploy': {
+        alias: 'a',
+        type: yargs_1.boolean,
+    },
+    dry: {
+        alias: 'd',
+        type: yargs_1.boolean,
+    },
+    'load-via': {
+        alias: 'l',
+        type: String,
+    },
+    'create-db': {
+        alias: 'c',
+        type: yargs_1.boolean,
+    },
+};
+exports.handler = async (argv) => {
+    for (const service of argv.service) {
+        const options = await (0, config_1.config)(service);
+        const adapter = await (0, adapter_1.default)(service, options);
+        await adapter.deploy({
+            autoUndeploy: argv.autoUndeploy,
+            dryRun: argv.dry,
+            loadMode: argv.loadVia,
+            createDb: argv.createDb,
+        });
+    }
+};
+//# sourceMappingURL=deploy.js.map
